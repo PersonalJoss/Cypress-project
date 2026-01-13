8e87922f1783ed2951171c4d650640320aee28d7
Zumbado, Jocselyn <jocselinz@hotmail.com>
2026-01-12 20:06:32 -0600

    Update main.yml file, cypress.config and Home_page.cy.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultBrowser: 'chrome',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
