a1d2e014ed92043128137bcb7b64e38ad21dbdda
Zumbado, Jocselyn <jocselinz@hotmail.com>
2026-01-19 10:36:50 -0600

    update test cases

--- cypress/e2e/Resources_page.cy.js
+++ cypress/e2e/Resources_page.cy.js
@@ -6,25 +6,25 @@ const HomePage = require('../pages/HomePage');
 describe('Resources Page Validation', () => {
 
   beforeEach(() => {
-    // Visita la página de Resources
+    // Visit the Resources page
     ResourcesPage.navigateToResourcesPage();
   });
 
   it('should display the correct title for the Resources page', () => {
-    // Verifica que el título de la página sea el esperado
+    // Verify that the page title is as expected
     ResourcesPage.elements.resourcesTitle().should('eq', 'Resources');
   });
 
   it('should have a link to the "Home" page', () => {
-    // Verifica que exista un enlace a la página principal ("Home")
+    // Verify that there is a link to the main page ("Home")
     ResourcesPage.elements.homeButton().should('be.visible').and('have.attr', 'href', '/home');
   });
 
   it('should navigate to the Home page when the "Home" link is clicked', () => {
-    // Haz clic en el enlace "Home"
+    // Click on the "Home" link
     ResourcesPage.elements.homeButton().click();
 
-    // Verifica que la URL haya cambiado a la página principal
+    // Verify that the URL has changed to the homepage.
     HomePage.validateHomePage();
   });
 
